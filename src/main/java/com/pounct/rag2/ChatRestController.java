a9652d9d4752ee7311f6679a167c2520e06267cf
Pounct <53088375+pounct@users.noreply.github.com>
2024-08-19 02:27:01 +0200

    Add files via upload

--- src/main/java/com/pounct/rag2/ChatRestController.java
+++ src/main/java/com/pounct/rag2/ChatRestController.java
@@ -1,6 +1,7 @@
 package com.pounct.rag2;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import reactor.core.publisher.Flux;
 @RestController
 public class ChatRestController {
     private ChatClient chatClient;
+    @Value("classpath:/prompts/system-message.st")
+    private String systemeMessageResource;
 
     public ChatRestController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
@@ -29,5 +32,17 @@ public class ChatRestController {
                 .user(question)
                 .stream().content();
         return response;
+    }
+
+    // with SM : system message
+
+    // with specific result
+    @GetMapping(value = "/resultatSM") // resultat en format json
+    public  ResultatSM resultatSM(String revieu){
+        return chatClient.prompt().system(systemeMessageResource).user(revieu).call().entity(ResultatSM.class);
+    }
+    @GetMapping(value = "/resultatSMSimple",produces = MediaType.TEXT_PLAIN_VALUE)
+    public  String resultatSMSimple(String revieu){
+        return chatClient.prompt().system(systemeMessageResource).user(revieu).call().content();
     }
 }
---
+++ src/main/java/com/pounct/rag2/ResultatSM.java
@@ -0,0 +1,7 @@
+package com.pounct.rag2;
+
+import groovy.lang.GString;
+
+public record ResultatSM(String par1, String par2, String par3) {
+
+}
